d103ccced61ccf019f49b5553e3cefa24cebd56d
anaiseishimwe <ishimweanais74@gmail.com>
2023-08-26 13:30:21 -0700

    rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int temp;
-	int i, j;
+	int i;
+	int t;
 
-    for (i = 0, j = n - 1; i < j; i++, j--)
-    {
-	    temp = a[i];
-
-	    a[i] = a[j];
-
-	    a[j] = temp;
-    }
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
 }
