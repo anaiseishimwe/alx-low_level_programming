c7e6b0c71d965094dc019f710cef1dc48802dcfb
anaiseishimwe <ishimweanais74@gmail.com>
2023-08-26 14:55:57 -0700

    string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,17 @@
 char *string_toupper(char *n)
 {
 	int i = 0;
+
 	while (n[i] != '\0')
 
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
 
 		{
-			n[i] = n[i] - ('a' - 'A');
+			n[i] = n[i] - 32;
 		}
 		i++;
 	}
+
 	return (n);
 }
