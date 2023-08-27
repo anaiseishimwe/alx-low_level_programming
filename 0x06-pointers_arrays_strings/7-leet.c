252c1ee2a82a37b82bcade5d3e8b0a5a1e33e8c4
anaiseishimwe <ishimweanais74@gmail.com>
2023-08-27 12:06:04 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - Encodes a string into 1337eleet
+ * @str: input value
+ *
+ * Return: n value
+ */
+char *leet(char *str)
+{
+	char *leetspeak = "AEOTLaeotl";
+	char *leetreplace = "430710317301";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leetspeak[j] != '\0'; j++)
+		{
+			if (str[i] == leetspeak[j])
+			{
+				str[i] = leetreplace[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
