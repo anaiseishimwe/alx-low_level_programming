bd96314d0514b8cd88a855f59fd7a8377f0dbd73
anaiseishimwe <ishimweanais74@gmail.com>
2023-08-30 15:16:08 -0700

    the errors wre soo real

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * factorial - returns the factorial of a number
  * @n: number to return the factorial from
@@ -6,8 +7,6 @@
  * Return: factorial of n
  */
 int factorial(int n)
-{
-	int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
